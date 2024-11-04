f0db572665e0e2aa58a6d2e32aec589206552e02
Dukoso <erickkirch85@gmail.com>
2024-11-04 18:10:37 -0300

    Ajustes

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const { dbConnection } = require('./database/config');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 dbConnection();
 
